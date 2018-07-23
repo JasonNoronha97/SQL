4cebcee540229c39e281da869cd1e2fa5525910b
Jason Noronha <noronha.jason97@gmail.com>
2018-07-23 17:06:47 +0530

    Commit Before Leaving

--- ANSWERS_AND_QUESTIONS.sql
+++ ANSWERS_AND_QUESTIONS.sql
@@ -266,31 +266,110 @@ FROM
     bank_info y
 WHERE
     w.customer_id = x.customer_id
-    AND y.ifsc_code = x.ifsc_code;
+    AND y.ifsc_code = x.ifsc_code 
+    AND w.address LIKE '%BANGALORE';
 
 
 --17.Write a query which will display customer id, bank name, branch name, ifsc code, registration date, 
 --activation date of the customers whose activation date is in the month of march (March 1'st to March 31'st).
---
+
+SELECT
+    z.customer_id,
+    bb.bank_name,
+    bb.branch_name,
+    bb.ifsc_code,
+    aa.registration_date 
+FROM
+    account_info aa,
+    customer_personal_info z,
+    bank_info bb
+WHERE
+    z.customer_id = aa.customer_id
+    AND bb.ifsc_code = aa.ifsc_code
+    AND EXTRACT(MONTH from aa.activation_date)=3;
+
 --18.Write a query which will calculate the interest amount and display it along with customer id, customer name, 
 --account number, account type, interest, and initial deposit amount.<BR>Hint :Formula for interest amount, 
 --calculate: ((interest/100) * initial deposit amt) with column name 'interest_amt' (alias)
---
+
+SELECT
+    cc.customer_id,
+    cc.customer_name,
+    dd.account_no,
+    dd.account_type,
+    dd.interest,
+    dd.initial_deposit,
+        (dd.interest/100*dd.initial_deposit) AS INTEREST_AMT
+FROM
+    account_info dd,
+    customer_personal_info cc
+WHERE
+    cc.customer_id = dd.customer_id;
+
 --19.Write a query to display the customer id, customer name, date of birth, guardian name, contact number, 
 --mail id, reference name who has been referenced by 'RAGHUL'.
---
+
+SELECT
+    ee.customer_id,
+    ee.date_of_birth,
+    ee.customer_name,
+    ee.contact_no,
+    ee.guardian_name,
+    ee.mail_id,
+    ff.reference_acc_name
+FROM
+    customer_reference_info ff,
+    customer_personal_info ee
+WHERE
+    ee.customer_id = ff.customer_id
+    AND ff.reference_acc_name='RAGHUL';
+
 --20."Write a query which will display the customer id, customer name and contact number with ISD code of 
 --all customers in below mentioned format.  Sort the result based on the customer id in descending order. 
 --<BR>Format for contact number is :  
 --<br/> ""+91-3digits-3digits-4digits""
 --<br/> Example: +91-924-234-2312
 --<br/> Use ""CONTACT_ISD"" as alias name."
---
+
+SELECT
+    customer_personal_info.customer_id,
+    customer_personal_info.customer_name,
+    customer_personal_info.contact_no,
+    '+91-' || SUBSTR(customer_personal_info.contact_no,0,3) 
+    || '-' || SUBSTR(customer_personal_info.contact_no,4,3) 
+    || '-' || SUBSTR(customer_personal_info.contact_no,7,4) 
+    AS CONTACT_ISD 
+FROM
+    customer_personal_info;
+
 --21.Write a query which will display account number, account type, customer id, customer name, date of birth, guardian name, 
 --contact no, mail id , gender, reference account holders name, reference account holders account number, registration date, 
 --activation date, number of days between the registration date and activation date with alias name "NoofdaysforActivation", 
 --bank name, branch name and initial deposit for all the customers.
---
+
+SELECT
+    hh.reference_acc_no,    ii.account_type,    ii.customer_id,    gg.customer_name,    gg.date_of_birth,
+    gg.guardian_name,    hh.reference_acc_name,    ii.account_no,
+    ii.registration_date,
+    ii.activation_date,
+    ii.activation_date - ii.registration_date AS NoofdaysforActivation,
+    ii.initial_deposit,
+    gg.gender,
+    gg.contact_no,
+    
+    gg.mail_id,
+    jj.bank_name,
+    jj.branch_name
+FROM
+    customer_reference_info hh,
+    customer_personal_info gg,
+    account_info ii,
+    bank_info jj
+WHERE
+    gg.customer_id = hh.customer_id
+    AND gg.customer_id = ii.customer_id
+    AND jj.ifsc_code = ii.ifsc_code;
+
 --22."Write a query which will display customer id, customer name,  guardian name, identification doc type,
 -- reference account holders name, account type, ifsc code, bank name and current balance for the customers 
 --who has only the savings account. 
